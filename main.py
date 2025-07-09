29e8e542fbc24e346bd5a6f6568b4106b99b5245
Lucas Aponso <s3896348@student.rmit.edu.au>
2025-07-09 11:50:58 +1000

    mod: changed this sender into a reciever

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pika
 from constants import rabbit_mq_server_addr
-
 connection = pika.BlockingConnection(pika.ConnectionParameters(rabbit_mq_server_addr))
 channel = connection.channel()
 
 channel.queue_declare(queue='hello')
 
-channel.basic_publish(exchange='', routing_key='hello', body='Hello RabbitMQ!')
-print(" [x] Sent 'Hello RabbitMQ!'")
-connection.close()
+def callback(ch, method, properties, body):
+    print(f" [x] Received {body}")
+
+channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.star
